c2ad1cb9eb68a767e68e81afe32814b748bf4e1d
v1kt0r77 <vk.g.ccvitin@gmail.com>
2024-05-07 10:46:04 -0300

    exercicio_2.py

---
+++ entrada_saida/exercicio_2.py
@@ -0,0 +1,8 @@
+#criar a variável que vai receber a entrada do valor
+metros = float(input("Digite o valor em metros: "))
+
+#calculando a medida
+centimetros = metros * 100
+
+# Mostrar o resultado na tela
+print(f"{metros:.2f} metros equivalem a {centimetros:.2f} centímetros.")
